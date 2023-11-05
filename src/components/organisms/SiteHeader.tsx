0a97a4336ab523d253600bf4884adbbba5f5ebd1
JBetoReyes <josereyes55@hotmail.com>
2023-11-04 18:18:45 -0600

    adding site header

---
+++ src/components/organisms/SiteHeader.tsx
@@ -0,0 +1,15 @@
+
+
+const SiteHeader = () => {
+    return (
+        <header className="site-header">
+            <div className="container">
+                <div className="site-header__inner">
+                    <img src="" alt="" className="logo" />
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default SiteHeader;
