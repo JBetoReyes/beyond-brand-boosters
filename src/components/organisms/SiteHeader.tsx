27dc9e0440efc470c9dce946eb76227ae544b169
JBetoReyes <josereyes55@hotmail.com>
2023-11-04 18:39:15 -0600

    adding site header component

--- src/components/organisms/SiteHeader.tsx
+++ src/components/organisms/SiteHeader.tsx
@@ -3,9 +3,18 @@
 const SiteHeader = () => {
     return (
         <header className="site-header">
-            <div className="container">
+            <div className="container" data-type="wide">
                 <div className="site-header__inner">
-                    <img src="" alt="" className="logo" />
+                    <img src="/images/logo.svg" alt="" className="logo" />
+                    <nav aria-label="primary navigation">
+                        <ul className="nav | flex-group">
+                            <li><a href="#">Pricing</a></li>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Features</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                            <li><a href="#">Careers</a></li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </header>
