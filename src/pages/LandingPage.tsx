c701894fa81253ac76f1727a49ca51ea94cc3426
JBetoReyes <josereyes55@hotmail.com>
2023-11-05 17:32:20 -0600

    adding about us section

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -16,7 +16,23 @@ const LandingPage = () => {
                             </div>
                         </div>
                         <div className="section-hero__image">
-                            <img src="/images/group-on-computer.webp" alt="A group three people seeing a computer" className="hero-image | border-radius-2" />
+                            <img className="hero-image | border-radius-2" src="/images/group-on-computer.webp" alt="A group three people seeing a computer" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="section about-us | bg-brand-primary-700 clr-neutral-000">
+                <div className="container">
+                    <div className="switcher">
+                        <div className="about-us__content | flow">
+                            <h2 className="heading-2 | clr-neutral-000">About Us</h2>
+                            <p>BrandBoosters was founded with the mission of <span className="fw-bold">helping businesses of all sizes</span> achieve their marketing goals by working closely with each client to <span className="fw-bold">develop a customized strategy that delivers result</span> </p>
+                            <div className="about-us__ctas">
+                                <a href="" className="button" data-type="accent">Learn more about us</a>
+                            </div>
+                        </div>
+                        <div className="about-us__image">
+                            <img className="round-top | bg-brand-primary-200" src="/images/girl-on-phone.webp" alt="girl on phone" />
                         </div>
                     </div>
                 </div>
