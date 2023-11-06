32cdcf2732870994412158cfa7e5f255f64391cf
JBetoReyes <josereyes55@hotmail.com>
2023-11-05 19:14:46 -0600

    creating a new site nav component

---
+++ src/components/organisms/SiteNav.tsx
@@ -0,0 +1,15 @@
+const SiteNav = () => {
+    return (
+        <nav aria-label="primary navigation">
+            <ul className="nav | flex-group">
+                <li><a href="#">Pricing</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Features</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Careers</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default SiteNav;
