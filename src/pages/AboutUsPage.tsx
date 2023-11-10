06beaf1e6b232241e6aa79da89585481af0f3b0f
JBetoReyes <josereyes55@hotmail.com>
2023-11-10 12:39:31 -0600

    adding hero section to about us page

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -1,6 +1,19 @@
+import "../sass/pages/about-us/index.scss";
+
 const AboutUsPage = () => {
     return (
-        <main>about us</main>
+        <main className="about-us-page">
+            <section className="section section-hero">
+                <div className="container" data-type="wide">
+                    <div className="section-hero__layout | flow" style={{ "--flow-spacer": "3.75rem" } as React.CSSProperties}>
+                        <h1 className="heading-1 | text-center">How we work</h1>
+                        <div className="section-hero__image-container">
+                            <img className="section-hero__image | border-radius-2" src="/beyond-brand-boosters/images/video-placeholder.webp" alt="A group of people having a good time" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
     );
 }
 
