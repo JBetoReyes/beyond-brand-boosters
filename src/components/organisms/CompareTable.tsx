963dac4e313d8a281b065e0c3415beadc0b71899
JBetoReyes <josereyes55@hotmail.com>
2023-11-15 18:24:32 -0600

    adding a second element title to the pricing table, so it's shown in the mobile version

--- src/components/organisms/CompareTable.tsx
+++ src/components/organisms/CompareTable.tsx
@@ -20,22 +20,25 @@ export type CompareTableProps<C extends string> = {
 
 const CompareTable = <C extends string>({ title, columns, dataRows, style }: CompareTableProps<C> & { style: React.CSSProperties }) => {
     return (
-        <div className="compare-table | scrollbar" style={style}>
-            <div className="compare-table__layout">
-                <h2 className="compare-table__title | heading-2 margin-block-end-11">{title}</h2>
-                {columns.map((column) => {
-                    return (
-                        <CompareTableHeader column={column} key={column.columnTitle} />
-                    )
-                })}
-                {dataRows.map((row, index) => {
-                    const isLastRow = index === dataRows.length - 1;
-                    return (
-                        <CompareTableRow row={row} columns={columns} isLastRow={isLastRow} key={row.rowTitle} />
-                    );
-                })}
+        <>
+            <h2 className="compare-table__title | heading-2 margin-block-end-11">{title}</h2>
+            <div className="compare-table | scrollbar" style={style}>
+                <div className="compare-table__layout">
+                    <h2 className="compare-table__title | heading-2 margin-block-end-11">{title}</h2>
+                    {columns.map((column) => {
+                        return (
+                            <CompareTableHeader column={column} key={column.columnTitle} />
+                        )
+                    })}
+                    {dataRows.map((row, index) => {
+                        const isLastRow = index === dataRows.length - 1;
+                        return (
+                            <CompareTableRow row={row} columns={columns} isLastRow={isLastRow} key={row.rowTitle} />
+                        );
+                    })}
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
